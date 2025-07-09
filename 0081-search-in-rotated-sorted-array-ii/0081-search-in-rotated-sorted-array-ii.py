b083cd5d768e714b48afcd0ace8cb6f1a438cdbe
jaygandhi48 <129257224+jaygandhi48@users.noreply.github.com>
2025-07-09 21:18:38 +0800

    Time: 0 ms (100%), Space: 18.2 MB (50.58%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -5,18 +5,24 @@ class Solution:
             mid = (l + r) // 2
             if nums[mid] == target:
                 return True
+
             # Skip duplicates
             if nums[l] == nums[mid] == nums[r]:
                 l += 1
                 r -= 1
+
+            # Left half is sorted
             elif nums[l] <= nums[mid]:
                 if nums[l] <= target < nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
+
+            # Right half is sorted
             else:
                 if nums[mid] < target <= nums[r]:
                     l = mid + 1
                 else:
                     r = mid - 1
+
         return False
